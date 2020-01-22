3d0c7a87761508589e229e1161f81c3539a84aa8
Tamires <tamires.lowande@gmail.com>
2020-01-22 17:12:14 -0500

    project set-up and first loop to display images on screen. CSS given

---
+++ main.js
@@ -0,0 +1,36 @@
+var cardsArray = [
+  { 'name': 'CSS', 'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true', },
+  { 'name': 'HTML', 'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true', },
+  { 'name': 'jQuery', 'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true', },
+  { 'name': 'JS', 'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true', },
+  { 'name': 'Node', 'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true', },
+  { 'name': 'Photo Shop', 'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true', },
+  { 'name': 'PHP', 'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true', },
+  { 'name': 'Python', 'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true', },
+  { 'name': 'Ruby', 'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true', },
+  { 'name': 'Sass', 'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true', },
+  { 'name': 'Sublime', 'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true', },
+  { 'name': 'Wordpress', 'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true', },
+];
+
+//______________________
+const gameGrid = cardsArray.concat(cardsArray)
+gameGrid.sort(() => {
+  return 0.5 - Math.random()
+})
+
+
+const game = document.getElementById('game-board');
+const grid = document.createElement('section')
+grid.setAttribute('class', 'grid')
+game.appendChild(grid)
+
+for (let [index, pic] of gameGrid.entries()) {
+  let card = document.createElement('div')
+  card.classList.add('card')
+  card.dataset.name = pic.name;
+  // card.dataset.name = cardsArray[index].name;
+  card.style.backgroundImage = `url(${pic.img})`
+  // card.style.backgroundImage = `url(${cardsArray[index].img})`
+  grid.appendChild(card)
+}
