a14dd6011e2cf8c6b05b112de9dd9cc8782da3b9
Tamires <tamires.lowande@gmail.com>
2020-01-22 18:41:43 -0500

    adds final effects setTimeout on cards, back and front of cards and animations, CSS given

--- main.js
+++ main.js
@@ -14,33 +14,51 @@ var cardsArray = [
 ];
 
 //______________________
+
+// duplicates an array and sort them differently when loading the page
 const gameGrid = cardsArray.concat(cardsArray)
 gameGrid.sort(() => {
   return 0.5 - Math.random()
 })
 
-
+//selects HTML div and appends a section that will contain the cards
 const game = document.getElementById('game-board');
 const grid = document.createElement('section')
 grid.setAttribute('class', 'grid')
 game.appendChild(grid)
 
+//loops over the array of images to create each card and append it  to the section
 for (let [index, pic] of gameGrid.entries()) {
   let card = document.createElement('div')
   card.classList.add('card')
   card.dataset.name = pic.name;
   // card.dataset.name = cardsArray[index].name;
-  card.style.backgroundImage = `url(${pic.img})`
-  // card.style.backgroundImage = `url(${cardsArray[index].img})`
+
+  //cards are not faced up anymore, but covered
+  let front = document.createElement('div')
+  front.classList.add('front')
+
+  let back = document.createElement('div')
+  back.classList.add('back')
+  back.style.backgroundImage = `url(${pic.img})`
+
   grid.appendChild(card)
+  card.appendChild(front)
+  card.appendChild(back)
+
+  //cards were always faced up
+  // card.style.backgroundImage = `url(${pic.img})`
+  // card.style.backgroundImage = `url(${cardsArray[index].img})`
+  // grid.appendChild(card)
 }
 
+// variables to store info on the game
 let firstGuess = ''
 let secondGuess = ''
-
 let count = 0
 let previousTarget = null
 
+//function that adds classes to the selected cards
 let match = () => {
   //makes an array with all the elements that have the class described on the parentesis
   let selected = document.querySelectorAll('.selected')
@@ -51,6 +69,7 @@ let match = () => {
   }
 }
 
+// funtion that resets the variables after 2 cards are selected
 const resetGuesses = () => {
   firstGuess = '';
   secondGuess = '';
@@ -64,28 +83,36 @@ const resetGuesses = () => {
   }
 }
 
-
+// event listener to set it all up when up to 2 cards are selected
 grid.addEventListener('click', (event) => {
+
   let clicked = event.target;
+
+  //if clicked on the container <section> or elements previously selected or matched, nothing should happen
   if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('match') || clicked.parentNode.classList.contains('selected')) {
     return
   }
+
+  //limits the clicks up to 2x
   if (count < 2) {
-    // clicked.classList.add('selected'); 
+    clicked.parentNode.classList.add('selected');
     count++
     if (count === 1) {
-      firstGuess = clicked.dataset.name
-      clicked.classList.add('selected')
+      firstGuess = clicked.parentNode.dataset.name
+      // clicked.classList.add('selected')
     } else {
-      secondGuess = clicked.dataset.name
-      clicked.classList.add('selected')
+      secondGuess = clicked.parentNode.dataset.name
+      // clicked.classList.add('selected')
     }
 
     if (firstGuess !== "" && secondGuess !== "") {
       if (firstGuess === secondGuess) {
-        match()
+        setTimeout(match, 1000)
       }
-      resetGuesses()
+      setTimeout(resetGuesses, 1000)
+      // setTimeout(() => {
+      //   resetGuesses()
+      // }, 500)
 
     }
 
